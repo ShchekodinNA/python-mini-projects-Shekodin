3cd6064cf0e6bc89147c8f80ba993e7a2d933751
ShchekodinNA <kacpercku1971@gmail.com>
2022-04-01 18:32:32 +0300

    Update main.py

--- 024_mail_merging/main.py
+++ 024_mail_merging/main.py
@@ -1,6 +1,3 @@
-# TODO: Create a letter using starting_letter.txt
-#   for each name in invited_names.txt
-
 with open("Input/Letters/starting_letter.txt") as file:
     letter_template = file.read()
 
@@ -12,26 +9,4 @@ for name in name_list:
     name = name.strip()
     with open(f"Output/ReadyToSend/{name}_congrats.txt", mode="w") as file:
         file.write(letter_template.replace("[name]", name))
-
-
-
-
-#   Replace the [name] placeholder with the actual name.
-
-
-
-
-
-
-
-
-#   Save the letters in the folder "ReadyToSend".
-
-
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
-
 
